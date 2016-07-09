6e51751457e0e424c4866927bb009fe53da46455
Adnan Gazi <adnan.gazi01@gmail.com>
2016-07-09 14:32:17 +0100

    serialize query params and print

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace ConsoleApplication
                 .Build();
 
             host.Run();
-            
 
         }
     }
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,17 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace aspnetcoreapp
 {
     public class Startup
     {
         public void Configure(IApplicationBuilder app)
-        {
+        {   
+
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+                var x = context.Request.QueryString.Value.Substring(1).Split('&');
+
+                var queryParams = new Dictionary<string, string>();
+
+                x.Select(s => s.Split('='))
+                    .ToList()
+                    .ForEach( ss => queryParams.Add(ss[0], ss[1]));
+
+                return context.Response.WriteAsync("Hello World! -- " + ToJson(queryParams));
             });
+        }
+
+        private string ToJson(Dictionary<string, string> dict){
+            return JsonConvert.SerializeObject(dict);
         }
     }
 }
