4b9a7ef5b388c300f16d2ffde3b1a3f9e2ed9ef5
Adnan Gazi <adnan.gazi01@gmail.com>
2016-07-09 13:44:32 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using aspnetcoreapp;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+            
+
+        }
+    }
+}
